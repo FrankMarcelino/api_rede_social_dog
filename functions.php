61b1f36af09582392b85b42c5d15c36439af566e
frankMarcelino <frank.silva.ti@gmail.com>
2024-10-21 12:09:50 -0300

    function remvove end point

--- functions.php
+++ functions.php
@@ -1,33 +1,39 @@
 <?php
-// Remove as rotas definidas pelo WordPress
 // remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+// remove endpoints
+add_filter('rest_endpoints', function ($endpoints) {
+  unset($endpoints['/wp/v2/users']);
+  unset($endpoints['/wp/v2/users/(?P<id>[\d]+)']);
+  return $endpoints;
+});
 
-//enpoints 
 $dirbase = get_template_directory();
+
 require_once $dirbase . '/endpoints/user_post.php';
 require_once $dirbase . '/endpoints/user_get.php';
 
 require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_get.php';
 require_once $dirbase . '/endpoints/photo_delete.php';
 
 require_once $dirbase . '/endpoints/comment_post.php';
 require_once $dirbase . '/endpoints/comment_get.php';
 
-// tratando as imagens grandes
+require_once $dirbase . '/endpoints/password.php';
+
+// trata imagens grandes
 update_option('large_size_w', 1000);
 update_option('large_size_h', 1000);
 update_option('large_crop', 1);
 
-// Modifica o prefixo da API de wp-json para json apenas
-// Necessário salvar os permalinks para dar um refresh nos URL's
-function change_api($slug) {
+function change_api() {
   return 'json';
 }
 add_filter('rest_url_prefix', 'change_api');
 
 function expire_token() {
-  return time() + (60*60*24);
+  return time() + (60 * 60 * 24);
 }
-add_action('rest_api_init', 'expire_token');
+add_action('jwt_auth_expire', 'expire_token');
 
 ?>
